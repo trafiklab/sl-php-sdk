2eef08fa6852616a3c3b23a817a84a263fb67d81
Bert <bert.marcelis@samtrafiken.se>
2019-05-07 15:37:23 +0200

    Use mocking instead of a dummy object

--- tests/Trafiklab/Sl/Model/SlTimeTableResponseTest.php
+++ tests/Trafiklab/Sl/Model/SlTimeTableResponseTest.php
@@ -5,7 +5,7 @@
 namespace Trafiklab\Sl\Model;
 
 use PHPUnit_Framework_TestCase;
-use Trafiklab\Common\Internal\WebResponseImpl;
+use Trafiklab\Common\Model\Contract\WebResponse;
 use Trafiklab\Common\Model\Enum\TimeTableType;
 
 class SlTimeTableResponseTest extends PHPUnit_Framework_TestCase
@@ -13,7 +13,7 @@ class SlTimeTableResponseTest extends PHPUnit_Framework_TestCase
     function testConstructor_validDepartureBoardJson_shouldReturnCorrectObjectRepresentation()
     {
         $validDepartures = json_decode(file_get_contents("./tests/Resources/Sl/validDeparturesReply.json"), true);
-        $dummyResponse = new WebResponseImpl("", [], "", 200, "");
+        $dummyResponse = $this->createMock(WebResponse::class);
         $departureBoard = new SlTimeTableResponse($dummyResponse, $validDepartures);
 
         self::assertNotNull($departureBoard->getTimetable());
@@ -27,7 +27,7 @@ class SlTimeTableResponseTest extends PHPUnit_Framework_TestCase
     {
         $validDepartures = json_decode(
             file_get_contents("./tests/Resources/Sl/validDeparturesReplyWithBoats.json"), true);
-        $dummyResponse = new WebResponseImpl("", [], "", 200, "");
+        $dummyResponse = $this->createMock(WebResponse::class);
         $departureBoard = new SlTimeTableResponse($dummyResponse, $validDepartures);
 
         self::assertNotNull($departureBoard->getTimetable());
